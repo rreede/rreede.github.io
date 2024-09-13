f953cd7140ffebf9278588f074c028c86cb61051
Rene Reede <rene.reede@gmail.com>
2024-09-13 07:13:33 +0300

    Feat: Create Project
    - Bootstrap React and Vite
    
    - Create maintenance page

---
+++ Projects/Portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <div className='main'>
+      <h1>Hi, the website is currently under maintenance, it will be back up again shortly!</h1>
+     <p>You can find me on: <a target='_blank' href="https://github.com/rreede">Github</a> and <a target='_blank' href="https://www.linkedin.com/in/rene-reede/">LinkedIn</a></p>
+    </div>
+  )
+}
+
+export default App
