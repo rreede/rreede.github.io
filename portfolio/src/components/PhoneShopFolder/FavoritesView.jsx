f479db3c8abf9ead4d4a3228858bb0d278b674be
Rene Reede <rene.reede@gmail.com>
2025-01-01 21:39:16 +0200

    Feat: Add favourite items

--- portfolio/src/components/PhoneShopFolder/FavoritesView.jsx
+++ portfolio/src/components/PhoneShopFolder/FavoritesView.jsx
@@ -1,55 +1,65 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import phones from './phones.json'
-
+import phones from "./phones.json";
 
 export default function FavoritesView() {
+    // Retrieve favorites from localStorage and parse as an array
+    const favorites = JSON.parse(localStorage.getItem("favoritesLocalStorage")) || [];
 
+    // Retrieve the shopping cart from localStorage or initialize as an empty array
+    const shoppingCartArray = JSON.parse(localStorage.getItem("shoppingCartLocalStorage")) || [];
 
-    const favorites = (localStorage.getItem('favoritesLocalStorage')) || [];
-
-    const shoppingCartArray = [];
-
+    // Add item to the shopping cart
     function addToShoppingCart(id) {
-
-        shoppingCartArray.push(id);
-
-        localStorage.setItem('shoppingCartLocalStorage', shoppingCartArray);
-
-        console.log(localStorage.getItem('shoppingCartLocalStorage'));
-
+        if (!shoppingCartArray.includes(id)) {
+            shoppingCartArray.push(id);
+            localStorage.setItem("shoppingCartLocalStorage", JSON.stringify(shoppingCartArray));
+            console.log("Updated Shopping Cart:", localStorage.getItem("shoppingCartLocalStorage"));
+        }
     }
 
-    const favoriteItems = phones.filter(item=>favorites.includes(item.id)).map(item=> (
+    // Remove item from favorites
+    function removeFromFavourites(id) {
+        const updatedFavorites = favorites.filter((favoriteId) => favoriteId !== id);
+        localStorage.setItem("favoritesLocalStorage", JSON.stringify(updatedFavorites));
+        window.location.reload(); // Optionally trigger a reload to reflect the updated state
+    }
 
+    // Filter and map favorite items
+    const favoriteItems = phones
+        .filter((item) => favorites.includes(item.id))
+        .map((item) => (
+            <div className="favoritesContainer" key={item.id}>
+                <img src={`/phone-images/${item.img}`} alt={item.name} />
+                <div className="info">
+                    <p>{item.name}</p>
+                    <strong>{item.price}$</strong>
+                    <button
+                        onClick={() => addToShoppingCart(item.id)}
+                        className="addToShoppingCart"
+                    >
+                        Add to shopping cart
+                    </button>
+                    <button
+                        onClick={() => removeFromFavourites(item.id)}
+                        className="removeFromFavorites"
+                    >
+                        Remove from favorites
+                    </button>
+                </div>
+            </div>
+        ));
 
-        <div className="favoritesContainer" key={item.id}>
-        <img src={`/phone-images/${item.img}`} alt={item.name} />
-        <div className="info">
-        <p>{item.name}</p>
-        <strong>{item.price}$</strong>
-        <button onClick={()=> addToShoppingCart(item.id)} className='addToShoppingCart'>Add to shopping cart</button>
-        </div>
-     </div>
-
-    ));
-       
-
-
-    return(
+    return (
         <>
-
-
-       <Header/>
-       <main>
-        <div className="wrapper">
-        <h1>Favorites</h1>
-
-        {favoriteItems.length > 0 ? favoriteItems :  <p>No favorites yet!</p>}
-        </div>
-       </main>
-           
-            <Footer/>
+            <Header />
+            <main>
+                <div className="wrapper">
+                    <h1>Favorites</h1>
+                    {favoriteItems.length > 0 ? favoriteItems : <p>No favorites yet!</p>}
+                </div>
+            </main>
+            <Footer />
         </>
-    )
+    );
 }
--- portfolio/src/components/PhoneShopFolder/ProductView.jsx
+++ portfolio/src/components/PhoneShopFolder/ProductView.jsx
@@ -54,7 +54,7 @@ export default function ProductView() {
                     <h1>{Product.specs}</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac massa lacus. Ut dolor nulla, eleifend egestas suscipit at, tempus ut erat. Cras mattis dolor ipsum, a posuere ante scelerisque ornare. Donec interdum volutpat posuere. Donec condimentum justo et accumsan iaculis. Vestibulum finibus ex vestibulum ornare efficitur. Nulla tempor erat ac ornare aliquet. Nunc commodo fermentum lacinia. Vivamus pretium elit mollis nisi luctus pellentesque. Morbi sem mi, facilisis in risus a, molestie porttitor tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
 
-                   {addedVisible && <div className="product-added-to-shopping-cart">Succesfully added to shopping cart <Link to='../phone-shop/shopping-cart-view-1'>Go to shopping cart</Link></div>} 
+                   {addedVisible && <div className="product-added-to-shopping-cart">Successfully added to shopping cart<br/> <Link className='go-shopping-cart-button' to='../phone-shop/shopping-cart-view-1'>Go to shopping cart</Link></div>} 
                    
 
                     <button onClick={()=> {addToShoppingCart(id); test()} } className='addToShoppingCart'>Add to shopping cart</button>
