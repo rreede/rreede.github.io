2e5a0212b404ee1a8bc047ade46e8571f5c6748b
Rene Reede <rene.reede@gmail.com>
2025-01-11 15:39:34 +0200

    Feat: Redesign layout
    - Add 3 new portfolio projects
    - Redesign layout

--- portfolio/src/MultiFilters.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect,useState } from "react"
-import items from 'phones.json'
-
-export default MultiFilters() {
-const [selectedFilters, setSelectedFilters] = useState([]);
-const [filteredItems, setFilteredItems] = useState(items)
-let filters = ['Phones','TVS', 'Watches']
-
-}
